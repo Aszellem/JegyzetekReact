3d13d12f612ed172b04843651799621e318fdd14
Aszellem <aszellemek@gmail.com>
2022-01-29 14:20:59 +0100

    first

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+       <div className="note">
+           <h1>Jegyzet címe</h1>
+           <p>Jegyzet szövege</p>
+       </div>
+    );
+}
+
+export default Note;
